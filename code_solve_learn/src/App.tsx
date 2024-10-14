112bad6046efac5d72150b8857576526fc63f958
Brian Dobbs <bdobbs@sketchdev.io>
2024-10-14 12:58:05 -0500

    Add router, routes, about and home pages

--- code_solve_learn/src/App.tsx
+++ code_solve_learn/src/App.tsx
@@ -1,12 +1,33 @@
 import "./styles.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="tw-flex tw-h-screen tw-justify-center">
       <div className="tw-flex tw-flex-col tw-justify-between">
         <Header />
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
 
         <Footer />
       </div>
